5134eb7fcb8d719b0a528607fda0779aa9104162
Yad Smood <1415488+ysmood@users.noreply.github.com>
2024-10-28 17:08:07 +0800

    sync version

--- sync-version.js
+++ sync-version.js
@@ -3,13 +3,13 @@ import { execSync } from "child_process";
 
 const version = JSON.parse(readFileSync('package.json')).version;
 
-["./devtools-ui/package.json"].forEach(p => {
+["./devtools-ui/package.json", "./chrome-extension/package.json"].forEach(p => {
 	const info = JSON.parse(readFileSync(p, "utf8"))
 	info.version = version
 	info.dependencies["stalo"] = "^" + version
 
 	writeFileSync(p, JSON.stringify(info, null, 2))
-
-	execSync(`git add .`, { stdio: 'inherit' })
-	execSync(`npm i`, { stdio: 'inherit' })
 })
+
+execSync(`npm i`, { stdio: 'inherit' })
+execSync(`git add .`, { stdio: 'inherit' })
